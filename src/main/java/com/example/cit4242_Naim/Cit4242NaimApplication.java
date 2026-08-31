06055976a7c37f1630bf0a850ea65d4c7fcade70
Naim <ergenekon03@mail.ru>
2026-08-31 12:40:30 +0500

    Add Maven wrapper and project structure

---
+++ src/main/java/com/example/cit4242_Naim/Cit4242NaimApplication.java
@@ -0,0 +1,13 @@
+package com.example.cit4242_Naim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cit4242NaimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cit4242NaimApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cit4242_Naim/Cit4242NaimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cit4242_Naim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cit4242NaimApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
